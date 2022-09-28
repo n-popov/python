d92985d5dc60afd60436e3c515827358093d2c34
niki <popov.nv@phystech.edu>
2022-09-29 01:09:55 +0300

    web directory

---
+++ seminarweb/crypto/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('some_path/', views.some_function),
+    path('price/<str:currency>/', views.price),
+    path('price/<str:currency>/<str:base>/', views.price),
+]
